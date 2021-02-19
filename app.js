4fb6c571b8c3eee6923713912c55370ebcc130a4
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-19 14:36:38 -0500

    add: fixed navbar with js and css

--- app.js
+++ app.js
@@ -23,7 +23,25 @@ navToggle.addEventListener('click', function () {
   }
 });
 
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 // ********** fixed navbar ************
+window.addEventListener('scroll', function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
 
 // ********** smooth scroll ************
 // select links
